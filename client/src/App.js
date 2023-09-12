28d99aa1a38da242d964c5be8158b5829eb0140f
lukhanyoVena808 <lookmane001@gmail.com>
2023-09-12 10:35:54 +0200

    fixed client

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Home from "./pages/Home";
+
 
 
 function App() {
   return (
     <div className="App">
-     
+     <Router>
+      <Routes>
+        <Route path="/" Component={Home} />
+      </Routes>
+     </Router>
     </div>
   );
 }
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,31 +1,31 @@
 import React from 'react';
+import '../App.css';
 import axios from "axios";
 import { useEffect, useState } from 'react';
 
 function Home() {
 
   const [listOfPosts, setlistOfPosts] = useState([]); //keeps current data
-
   useEffect( () =>{
       //gets the data from the SERVER
-      axios.get("htpp://localhost:5002/posts").then((response) =>{
-        setlistOfPosts(resonse.data);
+      axios.get("http://localhost:5002/posts").then((response) =>{
+        setlistOfPosts(response.data);
       });
   }, []);
 
 
   return (
+   
     <div className="App">
       {listOfPosts.map((value, key)=>{
-        return 
-        <div className='post'>
+        return <div className='post'>
           <div className='title'>{value.postText}</div>
           <div className='body'>{value.postText}</div>
           <div className='footer'>@{value.username}</div>
           <div className='date'>Date: {value.updatedAt}</div>
-          </div>
+        </div>
 
-      })};
+      })}
     </div>
   )
 }
